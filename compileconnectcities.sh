99c7a16a7c4fa1a7499273dee2dcf527aa3e3331
chris <44552743+return5@users.noreply.github.com>
2020-03-21 02:53:09 -0500

    Add files via upload

--- compileconnectcities.sh
+++ compileconnectcities.sh
@@ -14,32 +14,33 @@ printHelp() {
 	printf "-help  --help 	prints this page then exits\n"
 }
 
-#checks if GCC is installed, and if so it compiles the game with given flags.  if not installed then gives error message and exits.
+#use GCC to compile the game
 useGCC() {
-	(gcc -v >/dev/null 2>&1 && 
-	printf "using GCC\n" &&
-	gcc connectcities.c -orogueclone -Wall -Wextra -O2 -lm -Iheaders -finline-functions -Wswitch-enum -lncurses -std=gnu11 -oconnect_cities ) ||
-	printf "error, gcc seems to not be installed.\n"
+	printf "using GCC\n"
+	gcc connectcities.c -orogueclone -Wall -Wextra -O2 -Iheaders -finline-functions -Wswitch-enum -lncurses -std=gnu11 -oconnect_cities
 }
 
 #compiles game using Clang
 useClang() {
 	printf "using Clang\n"
-	clang connectcities.c -Iheaders -Wall -Wextra -O2 -lm -lncurses -finline-functions -Wswitch-enum -std=gnu11 -oconnect_cities   
+	clang connectcities.c -Iheaders -Wall -Wextra -O2 -lncurses -finline-functions -Wswitch-enum -std=gnu11 -oconnect_cities   
 }
 
 main() {
-	case "$1" in
-		-help | --help)
-			printHelp && exit
-			;;
-		-c | --clang)
-			(clang -v >/dev/null 2>&1 && useClang) || (printf "falling back to GCC.\n" && useGCC)
-			;;
-		*)
-			useGCC
-			;;
-	esac
+    case "$1" in                #check argument of positional parameter 1
+        -help | --help)         #if argument passed to script is the help flag
+            printHelp && exit   #run the help function then exit
+            ;;
+        -c | --clang)           #if argument passed is the clang flag
+            #check if clang is installed, if it is, call the useClang function, other wise fallback to GCC
+            (clang -v >/dev/null 2>&1 && useClang) || (printf "falling back to GCC.\n" && (gcc -v >/dev/null 2>&1 && useGCC) || printf "error, make sure GCC is installed.\n")   
+            ;;
+        *)
+            #checks to see if gcc is installed, and if so call useGCC, otherwise print error
+            (gcc -v >/dev/null 2>&1 && useGCC) || printf "error, make sure GCC is installed.\n"
+            ;;
+    esac
 }
 
+#call main function and pass it positonal paramter 1
 main "$1"
