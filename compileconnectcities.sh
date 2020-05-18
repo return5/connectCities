ca8baabade42cba2529cbe6317618222311bb065
chris <44552743+return5@users.noreply.github.com>
2020-05-18 17:03:02 -0500

    Add files via upload

--- compileconnectcities.sh
+++ compileconnectcities.sh
@@ -17,13 +17,13 @@ printHelp() {
 #use GCC to compile the game
 useGCC() {
     printf "%s\n" "using GCC"
-    gcc -Wall -Wextra -O2 -Iheaders -finline-functions -Wswitch-enum -lncurses -std=gnu11 -oconnect_cities
+    gcc connectcities.c -Wall -Wextra -O2 -Iheaders -lm -finline-functions -Wswitch-enum -lncurses -std=gnu11 -oconnect_cities
 }
 
 #compiles game using Clang
 useClang() {
     printf "%s\n" "using Clang"
-    clang -Iheaders -Wall -Wextra -O2 -lncurses -finline-functions -Wswitch-enum -std=gnu11 -oconnect_cities   
+    clang connectcities.c -Iheaders -Wall -Wextra -O2  -lm -lncurses -finline-functions -Wswitch-enum -std=gnu11 -oconnect_cities   
 }
 
 main() {
@@ -37,7 +37,7 @@ main() {
             ;;
         *)
             #checks to see if gcc is installed, and if so call useGCC, otherwise print error
-            (gcc -v >/dev/null 2>&1 && useGCC) || printf "%S\n" "error, make sure GCC is installed."
+            (gcc -v >/dev/null 2>&1 && useGCC) || printf "%s\n" "error, make sure GCC is installed."
             ;;
     esac
 }
